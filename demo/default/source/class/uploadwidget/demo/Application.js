5543b025353d8ee9987f2876c760b8abba4cb9f6
Tobias Oetiker <tobi@oetiker.ch>
2010-12-14 05:59:17 +0000

    use clear and not set value

--- demo/default/source/class/uploadwidget/demo/Application.js
+++ demo/default/source/class/uploadwidget/demo/Application.js
@@ -93,7 +93,7 @@ qx.Class.define("uploadwidget.demo.Application",
 
       form.addListener('completed',function(e) {
         alert('completed');
-        file.setFieldValue('');
+        form.clear();
         var response = this.getIframeHtmlContent();
         alert('response:'+response);
       });
