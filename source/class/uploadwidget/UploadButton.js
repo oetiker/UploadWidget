e92313a1286ec105837110aeb0f27aa8e109b0f6
Tobias Oetiker <tobi@oetiker.ch>
2015-02-10 13:55:05 +0000

    fix mouse -> pointer migration issue

--- source/class/uploadwidget/UploadButton.js
+++ source/class/uploadwidget/UploadButton.js
@@ -129,7 +129,12 @@ qx.Class.define("uploadwidget.UploadButton",
     capture : qx.core.Environment.select("engine.name",
     {
       "mshtml" : function() {
-        this.__mouseUpListenerId = this.getApplicationRoot().addListenerOnce("mouseup", this._onMouseUp, this);
+        if (this._onPointerUp){
+          this.__pointerUpListenerId = this.getApplicationRoot().addListenerOnce("pointerup", this._onPointerUp, this);
+        }
+        else {
+          this.__pointerUpListenerId = this.getApplicationRoot().addListenerOnce("mouseup", this._onMouseUp, this);
+        }
       },
 
       "default" : function() {
@@ -271,8 +276,8 @@ qx.Class.define("uploadwidget.UploadButton",
   {
     this.__inputEl = null;
     
-    if (this.__mouseUpListenerId) {
-      this.getApplicationRoot().removeListenerById(this.__mouseUpListenerId);
+    if (this.__pointerUpListenerId) {
+      this.getApplicationRoot().removeListenerById(this.__pointerUpListenerId);
     }
   }
 });
